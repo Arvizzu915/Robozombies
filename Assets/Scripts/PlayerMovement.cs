24edde15c3f88e5358606d977a97469bca06c6ba
Alejandro Garcia Arvizu <alexgar0204@gmail.com>
2024-10-17 00:09:06 -0600

    Player Movement walk jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] Rigidbody rb;
+
+    [SerializeField] float walkingSpeed, jumpForce, desacceleration;
+
+    private Vector3 moveDirection;
+
+    private bool grounded = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +24,77 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+
+        //Walk
+        if (grounded && (MathF.Abs(rb.velocity.x) < 5f && MathF.Abs(rb.velocity.z) < 5f))
+        {
+            rb.AddForce(moveDirection * walkingSpeed, ForceMode.Acceleration);
+        }
+
+
+        if (grounded && moveDirection.x == 0f)
+        {
+            if (rb.velocity.x != 0f) 
+            {
+                rb.AddForce(rb.velocity.x * desacceleration, 0f, 0f, ForceMode.Acceleration);
+            }
+        }
+
+        if (grounded && moveDirection.z == 0f)
+        {
+            if (rb.velocity.z != 0f)
+            {
+                rb.AddForce(0f, 0f, rb.velocity.z * desacceleration, ForceMode.Acceleration);
+            }
+        }
+    }
+
+    //Player Actions
+    public void Jump(InputAction.CallbackContext callbackContext)
+    {
+        if (grounded && callbackContext.performed)
+        {
+            rb.velocity = new Vector3 (rb.velocity.x, 0f, rb.velocity.z);
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    public void Move(InputAction.CallbackContext callbackContext)
+    {
+        if (grounded && callbackContext.performed)
+        {
+            moveDirection = callbackContext.ReadValue<Vector3>();
+        }
+    }
+
+    //Collisions
+    private void OnCollisionStay(Collision collision)
+    {
+        
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            grounded = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            grounded = false;
+        }
     }
 }
