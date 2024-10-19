be1c652a74adda1895859d86783e9c79a073007d
Alejandro Garcia Arvizu <alexgar0204@gmail.com>
2024-10-19 10:52:26 -0600

    Knee Slide

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -12,9 +13,13 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] float walkingSpeed, jumpForce, runningSpeed;
 
+    [SerializeField] CapsuleCollider standingCollider, crouchingCollider;
+    [SerializeField] Transform standingHeight, crouchingHeight;
+    [SerializeField] GameObject head;
+
     private Vector3 moveDirection, localVelocity;
 
-    private bool grounded = true, running = false;
+    private bool grounded = true, running = false, crouching = false, kneeSliding = false;
     private float velocityX, velocityZ;
 
     // Start is called before the first frame update
@@ -35,34 +40,68 @@ public class PlayerMovement : MonoBehaviour
         {
             running = false;
         }
+
+        //Crouching
+        if (!crouching && !kneeSliding)
+        {
+            head.transform.localPosition = standingHeight.localPosition;
+            standingCollider.enabled = true;
+            crouchingCollider.enabled = false;
+        }
+        else
+        {
+            head.transform.localPosition = crouchingHeight.localPosition;
+            crouchingCollider.enabled = true;
+            standingCollider.enabled = false;
+        }
     }
 
     private void FixedUpdate()
     {
 
         //Move
+        Move();
+
+        //Rotate player to camera
+        RotatePlayerTowardsCamera();
+    }
+
+    //Funciones
+    private void Move()
+    {
         localVelocity = transform.InverseTransformDirection(rb.velocity);
 
-        if (grounded && !running)
+        if (grounded)
         {
-            velocityX = moveDirection.x * walkingSpeed / 2;
-            velocityZ = moveDirection.z * walkingSpeed;
+            if (!running && !kneeSliding)
+            {
+                velocityX = moveDirection.x * walkingSpeed / 2;
+                velocityZ = moveDirection.z * walkingSpeed;
+            }
+            else if (running && !kneeSliding)
+            {
+                velocityX = moveDirection.x * runningSpeed / 3;
+                velocityZ = moveDirection.z * runningSpeed;
+            }
+            else if (kneeSliding)
+            {
+                velocityX = moveDirection.x * runningSpeed / 7;
+                velocityZ = runningSpeed * 1.6f;
+            }
         }
-        else if (grounded && running) 
+
+        if (!grounded)
         {
-            velocityX = moveDirection.x * runningSpeed / 3;
-            velocityZ = moveDirection.z * runningSpeed;
-        }
-        
-        if (!grounded && !running)
-        {
-            velocityX = moveDirection.x * walkingSpeed / 3;
-            velocityZ = moveDirection.z * walkingSpeed / 1.5f;
-        }
-        else if (!grounded && running)
-        {
-            velocityX = moveDirection.x * runningSpeed / 3;
-            velocityZ = moveDirection.z * runningSpeed / 1.5f;
+            if (!running)
+            {
+                velocityX = moveDirection.x * walkingSpeed / 3;
+                velocityZ = moveDirection.z * walkingSpeed / 1.5f;
+            }
+            else
+            {
+                velocityX = moveDirection.x * runningSpeed / 3;
+                velocityZ = moveDirection.z * runningSpeed / 1.5f;
+            }
         }
 
 
@@ -70,16 +109,11 @@ public class PlayerMovement : MonoBehaviour
         localVelocity.z = velocityZ;
 
         rb.velocity = transform.TransformDirection(localVelocity);
-
-
-        //Rotate player to camera
-        RotatePlayerTowardsCamera();
     }
 
-    //Funciones
     private void RotatePlayerTowardsCamera()
     {
-        if (mainCamera != null && rb != null)
+        if (mainCamera != null && rb != null && !kneeSliding)
         {
             Vector3 cameraForward = mainCamera.transform.forward;
             cameraForward.y = 0f; // Ignore the y-axis rotation
@@ -90,6 +124,13 @@ public class PlayerMovement : MonoBehaviour
                 rb.MoveRotation(newRotation);
             }
         }
+    }
+
+    IEnumerator KneeSlide()
+    {
+        kneeSliding = true;
+        yield return new WaitForSeconds(.6f);
+        kneeSliding = false;
     }
 
     //Player Actions
@@ -115,6 +156,23 @@ public class PlayerMovement : MonoBehaviour
         if (grounded && callbackContext.performed)
         {
             running = true;
+        }
+    }
+
+    public void Crouch(InputAction.CallbackContext callbackContext)
+    {
+        if (grounded && callbackContext.performed && !running)
+        {
+            crouching = true;
+        }
+        else if (grounded && callbackContext.performed && running)
+        {
+            StartCoroutine("KneeSlide");
+        }
+
+        if (grounded && callbackContext.canceled)
+        {
+            crouching = false;
         }
     }
 
