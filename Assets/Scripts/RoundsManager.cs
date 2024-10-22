4982327b5656433362e41febcb54a962b1cc164b
Alejandro Garcia Arvizu <alexgar0204@gmail.com>
2024-10-22 08:53:14 -0600

    Spawn zombies

---
+++ Assets/Scripts/RoundsManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundsManager : MonoBehaviour
+{
+    [SerializeField] public List<Transform> availableSpawns = new List<Transform>();
+
+    [SerializeField] GameObject zombie;
+
+    public int round = 1;
+
+    public int numberOfZombiesInRound = 10;
+
+    public int numberOfZombiesAlive = 0;
+
+    public int maxNumberOfZombiesAlive = 15;
+
+    public int numberOfZombiesKilled = 0;
+
+    public int numberOfZombiesSpawned = 0;
+
+    public bool roundWon = false;
+
+    public bool normalRound = true;
+
+    private Transform spawn;
+    private int spawnIndex;
+
+    private void Update()
+    {
+        if (normalRound)
+        {
+            if (numberOfZombiesKilled == numberOfZombiesInRound && !roundWon)
+            {
+                roundWon = true;
+                WinRound();
+            }
+
+            if (numberOfZombiesSpawned < numberOfZombiesInRound && numberOfZombiesAlive < maxNumberOfZombiesAlive)
+            {
+                spawnIndex = Random.Range(0, availableSpawns.Count);
+                numberOfZombiesSpawned++;
+                numberOfZombiesAlive++;
+                Instantiate(zombie, availableSpawns[spawnIndex].position, availableSpawns[spawnIndex].rotation);
+                float time = Random.Range(10, 25);
+                zombie.gameObject.GetComponent<ZombiesMovement>().waitTime = time;
+            }
+        }
+    }
+
+    public void WinRound()
+    {
+        round++;
+        numberOfZombiesInRound += 5;
+        numberOfZombiesKilled = 0;
+        roundWon = false;
+    }
+}
--- Assets/Scripts/ZombiesMovement.cs
+++ Assets/Scripts/ZombiesMovement.cs
@@ -7,17 +7,31 @@ public class ZombiesMovement : MonoBehaviour
 {
     private Transform player;
     [SerializeField] NavMeshAgent agent;
+    [SerializeField] MeshRenderer zombieRenderer;
+    private int speed;
+
+
+    public float waitTime, timeReference = 0;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        agent.speed = 0;
+        timeReference = Time.time;
         player = GameObject.Find("Player").transform;
-        agent.speed = 2;
+        speed = Random.Range(1, 5);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Time.time - timeReference > waitTime)
+        {
+            agent.speed = speed;
+            zombieRenderer.enabled = true;
+        }
+
         agent.destination = player.position;
     }
 }
